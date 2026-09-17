6618d8693742d3362efbdf8f75cf0aa8a4efaa9f
Thinha1 <Thinhanhdang014@gmail.com>
2026-09-17 16:24:49 +0700

    Report successful supplier autofill

--- resources/js/supplier-form.js
+++ resources/js/supplier-form.js
@@ -36,6 +36,7 @@ export default (initial, lookupUrl) => ({
             this.name = payload.data.name;
             this.address = payload.data.address;
             this.taxCode = payload.data.tax_code;
+            this.message = 'Đã điền tên và địa chỉ.';
         } catch {
             if (requestId === this.requestId) this.error = 'Không kết nối được dịch vụ tra cứu. Bạn vẫn có thể nhập thông tin thủ công.';
         } finally {
